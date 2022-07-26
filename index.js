8696d617a40b6b960bd2f6ba752328197811f4ac
Yusuke Ise <shosuke447@gmail.com>
2022-07-26 16:04:10 +0900

    corsの処理を変更

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const cors = require('cors')
 
 app.use(bodyParser.json())
 
 //CORSポリシーを無効にしている。
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+app.use(cors())
 
 app.get('/', function(req, res) {
   console.log(req.query.textQuery)
