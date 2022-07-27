8aa43ddd5299735d5f4e0af8f54ca41f775fc872
Yusuke Ise <shosuke447@gmail.com>
2022-07-27 14:55:01 +0900

    python-shellの記述方法を変更

--- index.js
+++ index.js
@@ -12,18 +12,18 @@ app.get('/', function(req, res) {
   console.log('back ok')
 
   var {PythonShell} = require('python-shell');
-  var pyshell = new PythonShell('./backend_py/make_cloud.py');  
-  console.log("req")
-  console.log(req.query.textQuery) //フロントエンドから受け取ったデータをconsole.logしている。
+  // var pyshell = new PythonShell('./backend_py/make_cloud.py');  
+  // console.log("req")
+  // console.log(req.query.textQuery) //フロントエンドから受け取ったデータをconsole.logしている。
 
   // pyshell.send(req.query.textQuery); //本コードからpythonコードに'req.query.textQuery'を入力データとして提供する 
 
   //pythonコード実施後にpythonから本コードにデータが引き渡される。
-  pyshell.on('message',  function (err, textQuery) {
-    console.log("return data")
+  PythonShell.run('./backend_py/make_cloud.py', null, function (err, textQuery) {
     if(err){
       console.log(err)
     }else{
+      console.log("return data")
       res.send({
         message: data   //pythonで実施した演算結果をフロントエンドに返している。
       })
